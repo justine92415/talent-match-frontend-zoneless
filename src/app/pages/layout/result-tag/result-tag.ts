670d1349216c3a1f9b87bcc1ae2ecb328737d6bc
justine92415 <justine92415@gmail.com>
2025-09-03 11:26:44 +0800

    ✨ feat: 新增搜尋結果頁面 - 標籤與關鍵字功能
    新增兩個搜尋結果頁面，分別為「result-tag」和「result-keyword」，以支援使用者根據標籤和關鍵字進行搜尋。這些頁面包含基本的元件結構和模板，為未來的功能擴展奠定基礎。

---
+++ src/app/pages/layout/result-tag/result-tag.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tmf-result-tag',
+  imports: [],
+  templateUrl: './result-tag.html',
+  styles: ``
+})
+export default class ResultTag {
+
+}

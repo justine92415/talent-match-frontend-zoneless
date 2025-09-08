2717c77fd1857c586fb21d4d1afa1b6129ef1827
justine92415 <justine92415@gmail.com>
2025-09-08 17:10:27 +0800

    ✨ feat(dashboard): 新增儀表板佈局與導航功能
    調整項目：
    1. 新增儀表板的 HTML 結構，包含個人管理、基本資訊及收藏課程的導航連結。
    2. 更新 TypeScript 檔案以引入所需的元件，確保導航功能正常運作。

--- src/app/pages/dashboard-layout/dashboard-layout.ts
+++ src/app/pages/dashboard-layout/dashboard-layout.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Header } from "@components/header/header";
+import { Footer } from "@components/footer/footer";
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'tmf-dashboard-layout',
-  imports: [],
+  imports: [Header, RouterOutlet, Footer, MatIcon, RouterLink, RouterLinkActive],
   templateUrl: './dashboard-layout.html',
   styles: ``
 })

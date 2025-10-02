e8a3c6c780f3c0ac31e6db168442bcb45e9589ea
justine92415 <justine92415@gmail.com>
2025-10-02 21:23:32 +0800

    ✨ feat(login, sign-up): 新增自動填充功能以改善使用者體驗
    調整項目：
    1. 在登入與註冊表單中新增 autocomplete 屬性，提升表單填寫效率。
    2. 更新驗證器中的錯誤訊息，將「Email is invalid」改為「信箱格式錯誤」，以符合中文使用者習慣。

--- src/share/validator.ts
+++ src/share/validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidationErrors, Validators } from '@angular/forms';
 
 export function emailValidator(ctrl: AbstractControl): ValidationErrors | null {
   if (Validators.email(ctrl)) {
-    return { error: 'Email is invalid' };
+    return { error: '信箱格式錯誤' };
   }
   return null;
 }
